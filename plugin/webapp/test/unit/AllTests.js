29bc2a4c038f7237bb6325118a983f65a54620cc
Enric Castella Gonzalez <e_castella@hotmail.com>
2019-12-29 23:01:36 +0000

    Versión final del plugin para modulo launchpad

---
+++ plugin/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"plugin/plugin/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ pluginTest/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"test/pluginTest/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
